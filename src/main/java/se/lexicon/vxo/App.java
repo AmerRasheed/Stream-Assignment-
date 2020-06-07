33e5a0143ba4499217f34c157616d1c2666227f2
AmerRasheed <59695079+AmerRasheed@users.noreply.github.com>
2020-06-07 18:06:26 +0200

    Stream Assignment (Incomplete version on Sunday June 7)

---
+++ src/main/java/se/lexicon/vxo/App.java
@@ -0,0 +1,42 @@
+package se.lexicon.vxo;
+
+
+import se.lexicon.vxo.model.Gender;
+import se.lexicon.vxo.model.Person;
+import se.lexicon.vxo.service.People;
+import se.lexicon.vxo.service.PeopleImpl;
+
+public class App
+{
+    public static void main( String[] args ) {
+        System.out.println("Hello World");
+
+        int counter=0;
+        for(Person p:People.INSTANCE.getPeople()) {
+            System.out.println(p);
+                counter++;
+        }
+            System.out.println(counter);
+      //  People.INSTANCE.getPeople();
+    }
+}
+
+
+// All persons with name Andersson
+
+/*   int counter=0;
+        for(Person p:People.INSTANCE.getPeople()) {
+            if (p.getLastName().equals("Andersson"))
+            counter++;
+        }
+            System.out.println(counter);*/
+
+// All females
+    /*
+int counter=0;
+        for(Person p:People.INSTANCE.getPeople()) {
+                if (p.getGender().equals(Gender.FEMALE))
+                counter++;
+                }
+                System.out.println(counter);
+*/
